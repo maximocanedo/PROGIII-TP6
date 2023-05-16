b32184d1e7d863da61fc0334a75d73f918038b47
Jean Esquen <jeanpierreesquen@gmail.com>
2023-05-16 01:28:29 -0300

    EJ 1 completo

--- PrimerEjercicio/PrimerEjercicio.aspx.cs
+++ PrimerEjercicio/PrimerEjercicio.aspx.cs
@@ -40,6 +40,45 @@ namespace TrabajoPractico6.PrimerEjercicio
             GrdProducto.PageIndex = e.NewPageIndex;
             CargarGridview();
         }
+
+        protected void GrdProducto_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string S_ID = ((Label)GrdProducto.Rows[e.RowIndex].FindControl("LBL_ID")).Text;
+            Producto pro= new Producto();
+            pro.Id = Convert.ToInt32(S_ID);
+            pro.PermanentlyDeleteFromDatabase();
+            CargarGridview();
+        }
+
+        protected void GrdProducto_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GrdProducto.EditIndex = -1;
+            CargarGridview();
+        }
+
+        protected void GrdProducto_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GrdProducto.EditIndex = e.NewEditIndex;
+            CargarGridview();
+        }
+
+        protected void GrdProducto_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string s_IDProducto = ((Label)GrdProducto.Rows[e.RowIndex].FindControl("LB_ID")).Text;
+            string s_NombreProducto = ((TextBox)GrdProducto.Rows[e.RowIndex].FindControl("TB_Nombre")).Text;
+            string s_Unidades = ((TextBox)GrdProducto.Rows[e.RowIndex].FindControl("TB_Cantidad")).Text;
+            string s_Precio = ((TextBox)GrdProducto.Rows[e.RowIndex].FindControl("TB_Precio")).Text;
+
+            Producto pro = new Producto();
+            pro.Id = Convert.ToInt32(s_IDProducto);
+            pro.Nombre = s_NombreProducto;
+            pro.CantidadPorUnidad = s_Unidades;
+            pro.PrecioUnitario = Convert.ToDouble(s_Precio);
+            pro.UpdateInDatabase();
+            GrdProducto.EditIndex = -1;
+            CargarGridview();
+        }
+
     }
 }
 
